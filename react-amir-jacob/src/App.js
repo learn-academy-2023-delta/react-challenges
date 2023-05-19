1eabb2b9545d613fbfe94721fbecb180f075e256
OakleyJacob <jboyoakley@gmail.com>
2023-05-19 13:55:06 -0500

    react challenges

---
+++ react-amir-jacob/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/header.js";
+import Profile from "./components/profile.js";
+import Footer from "./components/footer.js";
+import Favorites from "./components/favorites.js";
+
+const App = () => {
+  return (
+  <> 
+  <Header />
+  <Profile />
+  <Favorites />
+  <Footer />
+
+  </>
+)
+}
+
+export default App;
