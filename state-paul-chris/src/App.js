5e5ee5bcc01fa760e000a0463fec65924f212711
Phalco1 <elphalco1@gmail.com>
2023-05-22 18:07:05 -0500

    color boxes made

--- state-paul-chris/src/App.js
+++ state-paul-chris/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Box from "./components/box.js";
+import boxColor from "./components/box-color.js";
 
 const App = () => {
   return(
     <>
     <h1>Hello World!</h1>
-    <Box />
+      <div style={{margin: "25px", display: "flex"}}>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+    
     </>
   )
 }
--- state-paul-chris/src/components/box.js
+++ state-paul-chris/src/components/box.js
@@ -1,10 +1,32 @@
+// import React from "react"
+import boxColor from "./box-color"
+
+
+import React, { useState } from "react"
+
 const Box = () => {
-   return (
-   
-   <table style={{width: "50px", border: "1px", borderStyle: "solid"}}>
-        <tr style={{height: "50px"}}>
-        </tr>
+  var [color, setColor] = useState("white")
+
+    var colorPick = ["green", "red", "blue", "orange"]
+
+    var selector = Math.floor(Math.random() * colorPick.length)
+  var handleClick = () => {
+    setColor(color = colorPick[selector])
+  }
+
+  return (
+    <>
+    <table style={{width: "50px", border: "1px solid black"}}>
+           <tr>
+            <td style={{height: "50px", backgroundColor: color}} onClick={handleClick}>
+               {color}
+
+            </td> 
+             
+          </tr>
     </table>
-)}
+    </>
+  )
+}
 
 export default Box
