563803fcf57110c1fb1d0d2bd265aab0eabc1f7f
yungRicardo <rickyjespinoza@gmail.com>
2023-05-24 10:56:57 -0700

    ' '

--- state-ricky-brandon/src/components/AddBox.js
+++ state-ricky-brandon/src/components/AddBox.js
@@ -1,30 +1,25 @@
 import React, { useState } from 'react'
 import Box from './Box'
 
-const AddBox = () => {
+const AddBox = () => { // Create function to add boxes to the screen
     const [count, setCount] = useState([])
-
-    // const plusOneBox = () => {
-    //     const newBox = {
-    //         id: count.length + 1,
-    //         color: 'white'
-    //     }
-    // }
+// a react hook is stating an array 
+    const plusOneBox = () => {
+    // On click for the button and box
+        const newBox = {
+            id: count.length + 1,
+            color: 'white'
+        } // 
+        setCount(prevBoxes => [...prevBoxes, newBox])
+    }
 
     return (
-        <button onClick={<Box />}></button>
+        <>
+        <button onClick={plusOneBox}>Add Box</button>
+        {count.map(box => (
+            <Box key={box.id} color={box.color}/>
+        ))}
+        </>
     )
-}   
-
+}
 export default AddBox
-
-// return (
-//     <>
-//     <button onClick={AddBox}>Add Box</button>
-//     <>
-//     {count.map(box => (
-//         <Box key={box.id} color={box.color}/>
-//     ))}
-//     </>
-//     </>
-// )
