259ce4940a85697969f759dc14cac11a4605cbc6
OakleyJacob <jboyoakley@gmail.com>
2023-05-25 17:41:39 -0500

    our checkout app

--- functional-props-jesus-jacob/src/components/MenuItem.js
+++ functional-props-jesus-jacob/src/components/MenuItem.js
@@ -3,11 +3,12 @@ import React from "react";
 
 
 const MenuItem = (props) => {
-console.log(props)
+// console.log(props)
     return (
         <>
-        <p key = {props.id}> {props.name}Price: {props.price}</p>
-        <h1>Hello</h1>
+        <p> {props.name}Price: {props.price}</p>
+        <button onClick = {() => props.addToCart(props.index)}>Add the item to Cart</button>
+        
         
         </>
     )
